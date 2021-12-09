e135ce327f7b0234929cebceabc65760e7bec5ef
Thiago Nunes Monteiro <thiagomonteiro.trab@outlook.com>
2021-12-09 20:48:34 -0300

    Adicionando recyclerView ao projeto

---
+++ app/src/main/java/br/com/douglasmotta/mysubscribers/ui/subscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,30 @@
+package br.com.douglasmotta.mysubscribers.ui.subscriberlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import br.com.douglasmotta.mysubscribers.R
+import br.com.douglasmotta.mysubscribers.data.db.entity.SubscriberEntity
+import kotlinx.android.synthetic.main.subscriber_list_fragment.*
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+    private lateinit var viewModel: SubscriberListViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val subscriberListAdapter = SubscriberListAdapter(
+            listOf(
+                SubscriberEntity(1,"Thiago", "lala@contato.com"),
+                SubscriberEntity(2,"Roberto", "lele@contato.com")
+            )
+        )
+
+        recycler_subscribers.run {
+            setHasFixedSize(true)
+            adapter = subscriberListAdapter
+        }
+    }
+
+}
